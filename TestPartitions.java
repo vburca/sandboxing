df214d1906814456482183b7d4718153133e96fb
Vlad Burca <vladburca2005@gmail.com>
2016-06-03 15:16:01 -0400

    Small change to test commits

--- TestPartitions.java
+++ TestPartitions.java
@@ -5,7 +5,7 @@ public class TestPartitions {
 
   public static void main( String[] args ) {
     List<Integer> ints = new ArrayList<Integer>() {{
-      //add(1);
+      add(1);
       add(2);
       //add(3);
       add(4);
